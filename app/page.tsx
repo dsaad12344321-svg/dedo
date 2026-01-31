2e59b9ef3f9ed90953946d20c1b18882135c7364
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-01-31 20:09:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ async function getGames() {
 export default async function HomePage() {
   const games = await getGames();
       // Open smart link in new window
-        const smartLink = 'https://otieu.com/4/10544601'
+        const smartLink = 'https://otieu.com/4/10548489'
         window.open(smartLink, '_blank', 'noopener,noreferrer'); 
   return (
     <main className="p-6">
